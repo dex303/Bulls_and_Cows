01995240b0dc943d2f77ce1b0430229697d2ee15
mbbeu <mbbeurope@gmail.com>
2021-02-21 20:30:11 +0100

    Finish stage 3/7

--- Bulls and Cows/task/src/bullscows/Game.java
+++ Bulls and Cows/task/src/bullscows/Game.java
@@ -3,11 +3,64 @@ package bullscows;
 import java.util.Scanner;
 
 public class Game {
-    private final String SECRET_CODE = "9305";
+    private int codeLength;
+    private  String secretCode;
 
     public Game() {
-        // Scanner scanner = new Scanner(System.in);
-        // this.SECRET_CODE = scanner.nextLine();
+        takeCodeLength();
+        generateSecreteCode();
+        System.out.println("The random secret number is " + secretCode + ".");
+    }
+
+    private void takeCodeLength() {
+        Scanner scanner = new Scanner(System.in);
+        this.codeLength = scanner.nextInt();
+        if (codeLength > 10) {
+            System.out.println("Error: can't generate a secret number with a length of " + codeLength +
+                    " because there aren't enough unique digits.");
+            System.exit(0);
+        }
+    }
+
+    private void generateSecreteCode() {
+        long pseudoRandomNumber;
+        StringBuilder buildCode = new StringBuilder();
+        boolean again;
+
+        do {
+            secretCode = "";
+            pseudoRandomNumber = System.nanoTime();
+            again = false;
+            for (int i = 0; i < codeLength; i++) {
+                buildCode.append(Character.getNumericValue(Long.toString(pseudoRandomNumber).charAt(i)));
+            }
+            buildCode.reverse();
+
+            for (int i = 0; i < codeLength; i++) {
+                secretCode += buildCode.charAt(i);
+            }
+
+
+            // check secret code: first digit can't be 0, all digits should be unique
+            int repeatingDigits = 0;
+
+            if (secretCode.charAt(0) == 0) {
+                again = true;
+            } else {
+                for (int k = 0; k < secretCode.length(); k++) {
+                    for (int l = 0; l < codeLength; l++) {
+                        if (k != l && secretCode.charAt(k) == secretCode.charAt(l)) {
+                            repeatingDigits++;
+                        }
+                    }
+                    if (repeatingDigits > 0) {
+                        again = true;
+                        break;
+                    }
+                    repeatingDigits = 0;
+                }
+            }
+        } while (again);
     }
 
     public String guessCode() {
@@ -19,8 +72,8 @@ public class Game {
         int bulls = 0;
         int cows = 0;
 
-        for (int i = 0; i < SECRET_CODE.length(); i++) {
-            if (SECRET_CODE.charAt(i) == code.charAt(0)) {
+        for (int i = 0; i < secretCode.length(); i++) {
+            if (secretCode.charAt(i) == code.charAt(0)) {
                 if (i == 0) {
                     bulls++;
                 } else {
@@ -28,7 +81,7 @@ public class Game {
                 }
             }
 
-            if (SECRET_CODE.charAt(i) == code.charAt(1)) {
+            if (secretCode.charAt(i) == code.charAt(1)) {
                 if (i == 1) {
                     bulls++;
                 } else {
@@ -36,7 +89,7 @@ public class Game {
                 }
             }
 
-            if (SECRET_CODE.charAt(i) == code.charAt(2)) {
+            if (secretCode.charAt(i) == code.charAt(2)) {
                 if (i == 2) {
                     bulls++;
                 } else {
@@ -44,7 +97,7 @@ public class Game {
                 }
             }
 
-            if (SECRET_CODE.charAt(i) == code.charAt(3)) {
+            if (secretCode.charAt(i) == code.charAt(3)) {
                 if (i == 3) {
                     bulls++;
                 } else {
@@ -54,7 +107,7 @@ public class Game {
         }
 
         String prefix;
-        String suffix = "The secret code is " + SECRET_CODE + ".";
+        String suffix = "The secret code is " + secretCode + ".";
         if (bulls > 0 && cows > 0) {
             prefix = "Grade: " + bulls + " bull(s) and " + cows + " cow(s). ";
         } else if (bulls > 0) {
